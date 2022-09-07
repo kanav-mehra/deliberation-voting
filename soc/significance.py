ce368728992af4e87b31a51d62318a5b5914059c
Kanav Mehra <k3mehra@v1040-wn-rt-a-19-100.campus-dynamic.uwaterloo.ca>
2022-09-07 17:20:21 -0400

    changing significance test

--- soc/significance.py
+++ soc/significance.py
@@ -2,6 +2,7 @@ from scipy import stats
 import numpy as np
 from config import *
 import csv
+from scipy.stats import normaltest
 
 RESULT_PATH = 'results'
 
@@ -15,7 +16,10 @@ def test_significance(objectives_results):
             for rule_id in rules:
                 for i in range(len(setups)):
                     for j in range(i+1, len(setups)):
-                        r = stats.ttest_rel(objectives_results[setups[i]][obj][rule_id], objectives_results[setups[j]][obj][rule_id])
+                        data_1 = objectives_results[setups[i]][obj][rule_id]
+                        data_2 = objectives_results[setups[j]][obj][rule_id]
+                        
+                        r = stats.ttest_ind(data_1, data_2, equal_var=False)
                         writer.writerow([rule_id, setups[i], setups[j], r.pvalue])
                         #if r.pvalue > 0.05:
                         #    print(str.format("{} {} {} {} {}", obj, rule_id, setups[i], setups[j], r))
